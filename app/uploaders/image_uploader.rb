5bc46a89f400ef1ae55c98c251553e2b31a0b0c2
louisk146 <ldpohl1@sheffield.ac.uk>
2017-07-18 04:02:22 +0100

    squashed a bug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,8 +9,6 @@ if Rails.env.production?
     storage :file
   end
 
-  include CarrierWave::MimeTypes
-  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
