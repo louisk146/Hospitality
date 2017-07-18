1bdf8e110ceaf82c7f3f1fbd20ef7643ad8785d5
louisk146 <ldpohl1@sheffield.ac.uk>
2017-07-18 04:37:52 +0100

    changed some things

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,7 +31,7 @@ class EventsController < ApplicationController
 	private
 
 	def event_params
-		params.require(:event).permit  (:name, :date, :address1, :address2, :description, :country, :city, :postalcode, :email)
+	params.require(:event).permit  :name, :date, :address1, :address2, :description, :country, :city, :postalcode, :email
       t.string :socialmedia
       t.category :integer
       t.latitude :float
